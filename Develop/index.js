77097c8754d4e4b89ac8b93fa923703a6bd668d0
jakester101 <55941541+jakester101@users.noreply.github.com>
2023-02-04 01:52:55 -0500

    getting started

---
+++ Develop/index.js
@@ -0,0 +1,88 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+
+
+
+
+
+
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a description of your project:'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide installation instructions:'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage information:'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose a license for your project:',
+      choices: [
+        'Apache License 2.0',
+        'GNU General Public License v3.0',
+        'MIT License',
+        'Mozilla Public License 2.0'
+      ]
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Please provide contribution guidelines:'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Please provide test instructions:'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter your GitHub username:'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email address:'
+    }
+  ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    
+}
+
+
+
+
+// use async and wait to avoid async shenanigans 
+async function init() {
+        
+     let userData = await inquirer.prompt(questions);
+    
+
+   
+ }
+
+
+
+
+
+
+// Function call to initialize app
+init();
